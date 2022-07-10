5c98da38ed6c0b78e6cbc5e280e20624fb32000e
anilsson1997 <93950550+anilsson1997@users.noreply.github.com>
2022-07-10 18:58:22 +0200

    Update summarize.sh

--- summarize.sh
+++ summarize.sh
@@ -1,71 +1,125 @@
 #!/bin/bash
+
+############### global vars ###############
+
+# Unzipping the argument given file 
+SUPPORTZIPFILEPATHFULL=$1
+SUMMARYFILEPATH="/tmp/confluenceSummary.txt"
+WORKDIR="/tmp"
+
+
+#Helper Vars
 color='\e[1;34m'
 NC='\e[0m'
+SUPPORTZIPFILEPATH=$(echo "$SUPPORTZIPFILEPATHFULL" | cut -f 1 -d '.')
+TODAY=$(date '+%d_%m_%Y')
 
-function seperator (){
-echo "================================================================================"
-echo "$1"
-echo "================================================================================"
+################ functions ################
+
+function seperator() {
+    echo "================================================================================"
+    echo "$1"
+    echo "================================================================================"
 }
 
-echo  -e "${color} hello, $USER. This is the tets script to automate a summary for confluence support zip ${NC}"
+################## main ###################
+
+if [[ -z "${SUPPORTZIPFILEPATH}" ]]; then
+    echo "Support.Zip Filepath not set. Please start script again! supportZipSummary.sh <FilePath>"
+    exit 1
+fi
+
+echo -e "${color} hello, $USER. This is the tets script to automate a summary for confluence support zip ${NC}"
 echo -e "${color} It starts with unzipping the support zip file passed as an argument ${NC}"
-# Unzipping the argument given file
-fileName=$1
-unzip $fileName
+
+unzip $SUPPORTZIPFILEPATH -d $WORKDIR/confluence-support-$TODAY
+
+sleep 10
 
 # Starting to GREP the info
-seperator "The last Start of Confluence" > summary.txt
-grep -R -A 23 "Starting Confluence" application-logs |  tail -24 >> summary.txt
+seperator "The last Start of Confluence" >$SUMMARYFILEPATH
+grep -R -A 23 "Starting Confluence" $WORKDIR/confluence-support-$TODAY/application-logs | tail -24 >>$SUMMARYFILEPATH
+echo -e "">>$SUMMARYFILEPATH
 
-seperator "Build Info" >> summary.txt
-grep -R "baseUrl =" --exclude=*.sh | tail -1 >> summary.txt
-grep -R "home =" | tail -1 >> summary.txt
-grep -R "upTime =" | tail -1 >> summary.txt
-grep -R "devMode =" | tail -1 >> summary.txt
-grep -R "serverId =" | tail -1 >> summary.txt
-grep -R "supportEntitlementNumber =" | tail -1 >> summary.txt
-grep -R "installationDate =" | tail -1 >> summary.txt
-grep -R "version =" | tail -1 >> summary.txt
-seperator "Server Info" >> summary.txt
-grep -R "Application Server:" | tail -1 >> summary.txt
-grep -R "Servlet Version:" | tail -1 >> summary.txt
-seperator "Database Info" >> summary.txt
-grep -R "Database Dialect:" | tail -1 >> summary.txt
-grep -R "Database URL:" | tail -1 >> summary.txt
-grep -R "Database Driver Name:" | tail -1 >> summary.txt
-grep -R "Database Driver Version:" | tail -1 >> summary.txt
-grep -R "Database Version:" | tail -1 >> summary.txt
-grep -R "Database Latency (ms):" | tail -1 >> summary.txt
-seperator "Usage Info" >> summary.txt
-grep -R "totalSpaces =" | tail -1 >> summary.txt
-grep -R "localGroups =" | tail -1 >> summary.txt
-grep -R "localUsers =" | tail -1 >> summary.txt
-grep -R "globalSpaces =" | tail -1 >> summary.txt
-grep -R "allContent =" | tail -1 >> summary.txt
-grep -R "indexSize =" | tail -1 >> summary.txt
-grep -R "personalSpaces =" | tail -1 >> summary.txt
-grep -R "currentContent =" | tail -1 >> summary.txt
-seperator "System Info & JVM Stats" >> summary.txt
-grep -R "javaRuntime =" | tail -1 >> summary.txt
-grep -R "operatingSystem =" | tail -1 >> summary.txt
-grep -R "jvmVersion =" | tail -1 >> summary.txt
-grep -R "fileSystemEncoding =" | tail -1 >> summary.txt
-grep -R "javaVendor =" | tail -1 >> summary.txt
-grep -R "jvmImplementationVersion =" | tail -1 >> summary.txt
-grep -R "systemTimezone =" | tail -1 >> summary.txt
-grep -R "appServer =" | tail -1 >> summary.txt
-grep -R "favouriteCharacter =" | tail -1 >> summary.txt
-grep -R "tempDirectory =" | tail -1 >> summary.txt
-grep -R "jvmInputArguments =" | tail -1 >> summary.txt
-grep -R "jvmVendor =" | tail -1 >> summary.txt
-grep -R "userName =" | tail -1 >> summary.txt
-grep -R "javaVersion =" | tail -1 >> summary.txt
-grep -R "operatingSystemArchitecture =" | tail -1 >> summary.txt
-grep -R "systemLanguage =" | tail -1 >> summary.txt
-grep -R "workingDirectory =" | tail -1 >> summary.txt
+seperator "Build Info" $WORKDIR/confluence-support-$TODAY >>$SUMMARYFILEPATH
+grep -R "baseUrl =" $WORKDIR/confluence-support-$TODAY --exclude=*.sh | tail -1 | cut -d':' -f1- | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "home =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "upTime =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "devMode =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "server.id =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "supportEntitlementNumber =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "installationDate =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "version =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+echo -e "XML Value Tags: " >>$SUMMARYFILEPATH
+grep -R "clustered" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "confluence.cluster.peers" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+echo -e "">>$SUMMARYFILEPATH
 
+seperator "Server Info" >>$SUMMARYFILEPATH
+grep -R "Application Server:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "Servlet Version:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+echo -e "">>$SUMMARYFILEPATH
 
+seperator "Database Info" >>$SUMMARYFILEPATH
+grep -R "Database Dialect:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "Database URL:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "Database Driver Name:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "Database Driver Version:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "Database Version:" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "Database Latency (ms):" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+echo -e "">>$SUMMARYFILEPATH
 
-echo -e "${color} Summary created successfully.${NC}"
+seperator "Usage Info" >>$SUMMARYFILEPATH
+echo -e "XML Value Tags: " >>$SUMMARYFILEPATH
+grep -R "total.spaces" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "<groups>" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "<users>" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "active-users" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "licensed-users" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "license-type" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "license-period" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "global.spaces" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "allContent" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "indexSize" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "personal.spaces" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "content.current" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+echo -e "">>$SUMMARYFILEPATH
 
+seperator "System Info & JVM Stats" >>$SUMMARYFILEPATH
+grep -R "javaRuntime =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "operatingSystem =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "jvmVersion =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "fileSystemEncoding =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "javaVendor =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "jvmImplementationVersion =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "systemTimezone =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "appServer =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "favouriteCharacter =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "tempDirectory =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "jvmInputArguments =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "jvmVendor =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "userName =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "javaVersion =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "operatingSystemArchitecture =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "systemLanguage =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+grep -R "workingDirectory =" $WORKDIR/confluence-support-$TODAY | tail -1 | cut -d':' -f2- >>$SUMMARYFILEPATH
+echo -e "">>$SUMMARYFILEPATH
+
+seperator "System Health Check" >>$SUMMARYFILEPATH
+cat $WORKDIR/confluence-support-$TODAY/healthchecks/healthcheckResults.txt >>$SUMMARYFILEPATH
+
+rm -rf $WORKDIR/confluence-support-$TODAY
+echo -e "\n"
+echo -e " Summary created successfully at $SUMMARYFILEPATH"
+
+read -p "Do you want to open the file (Y/N)?" CHOICE
+if [ "$CHOICE" != "${CHOICE#[yY]}" ]; then
+
+    echo -e "${color}"
+    cat $SUMMARYFILEPATH
+
+else
+
+    echo -e "Please open file manually"
+
+fi
